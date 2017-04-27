2e536b16ee6b8ffd05ff93801fec90faadf5d222
Florent <fgoncalv@ladomain.net>
2017-04-27 16:00:10 +0200

    ajout Commandes directeur

--- components/com_cp3d/models/commandes.php
+++ components/com_cp3d/models/commandes.php
@@ -86,6 +86,29 @@ class Cp3dModelCommandes extends JModelList
 			case 2: //IMPRIMEUR
 				break;
 			case 3: //DIRECTEUR
+
+				$query	= $this->_db->getQuery(true);
+				$query->select('c.id, c.numCommande AS numCde, c.prixTotalTTC AS prix, cl.adr_rue AS adresse, c.idEtatCommande AS etat');
+				$query->from('#__cp3d_commande c');
+				// joint la table client à commande
+				$query->select('cl.nom AS nom_client, cl.prenom AS prenom_client')->join('LEFT', '#__cp3d_utilisateur AS cl ON c.idUtilisateur=cl.id');
+				//joint l'utilisateur joomla à l'utilisateur client
+				$query->select('ju.id AS joomla_uid')->join('LEFT', '#__users AS ju ON cl.email = ju.email');
+				$query->select('e.etat AS etat')->join('LEFT', '#__cp3d_etatcommande AS e ON c.idEtatCommande = e.id');
+				// filtre de recherche rapide textuelle
+				$search = $this->getState('filter.search');
+				if (!empty($search)) {
+					// recherche textuelle classique (sans préfixe)
+					$search = $this->_db->Quote('%'.$this->_db->escape($search, true).'%');
+					// Compile les clauses de recherche
+					$searches	= array();
+					$searches[]	= 'cl.nom LIKE '.$search;
+					$searches[]	= 'cl.prenom LIKE '.$search;
+					// Ajoute les clauses à la requête
+					$query->where('('.implode(' OR ', $searches).')');
+				// echo nl2br(str_replace('#__','cp3d_',$query));
+				}
+				return $query;
 				break;
 		}
 	}
