135473d4713003a2b9eb90ec6b29e4bb027371eb
Jérémie <lothar.lansing@gmail.com>
2017-04-27 17:25:37 +0200

    limitation de la vision des profil au seul utilisateur concerné. Le directeur a bien acces a tous les profils

--- components/com_cp3d/models/utilisateur.php
+++ components/com_cp3d/models/utilisateur.php
@@ -21,17 +21,49 @@ class Cp3dModelUtilisateur extends JModelItem
 	public function getItem($pk = null)
 	{
 		// Initialise l'id
-		$pk = (!empty($pk)) ? $pk : (int) $this->getState($this->_context.'.id');
-		//echo 'pk = '.$pk; //test de pk
-		// Si pas de données chargées pour cet id
+		$user = JFactory::getUser();
+
+				//Si l'utilisateur est directeur on recupere l'id dans l'url
+				if (in_array('11', $user->groups)){
+					if(!empty((int) $this->getState($this->_context.'.id'))){
+						$pk = (int) $this->getState($this->_context.'.id');
+					}
+				}
+				//Sinonrecupere l'id de la table utilisateur a partir de l'id de la table users générée par joomla
+				else {
+
+					$pk = $user->id;
+
+					$db = $this->getDbo();
+					$query	= $this->_db->getQuery(true);
+					$query-> SELECT ('u.id');
+					$query->from('#__cp3d_utilisateur u');
+					$query->join('inner','#__users ju ON u.email=ju.email');
+					$query->where('ju.id='.$pk);
+
+					$db->setQuery($query);
+
+					$pk = $db->loadObject()->id;
+					var_dump($pk);
+
+				}
+
+		/*
+		On recupere les indormations de l'utilisateur d'id $pk
+		*/
 		if (!isset($this->_item[$pk])) {
 			$db = $this->getDbo();
 			$query	= $this->_db->getQuery(true);
-		$query-> SELECT ('u.nom, u.prenom, u.alias, u.adr_rue, u.adr_ville, u.adr_CP, u.email, u.dateNaissance, u.created, e.raisonSociale, p.pays');
-		$query->from('#__cp3d_utilisateur u, #__cp3d_entreprise e, #__cp3d_pays p');
-		$query-> where('u.id='.$pk.'');
-		$query-> AND ('u.idEntreprise=e.id');
-		$query-> AND ('u.idPays=p.id');
+
+
+
+			$query-> SELECT ('u.nom, u.prenom, u.alias, u.adr_rue, u.adr_ville, u.adr_CP, u.email, u.dateNaissance, u.created, e.raisonSociale, p.pays');
+			$query->from('#__cp3d_utilisateur u, #__cp3d_entreprise e, #__cp3d_pays p');
+			$query-> where('u.id='.$pk.'');
+			$query-> AND ('u.idEntreprise=e.id');
+			$query-> AND ('u.idPays=p.id');
+
+
 
 			
 			$db->setQuery($query);
